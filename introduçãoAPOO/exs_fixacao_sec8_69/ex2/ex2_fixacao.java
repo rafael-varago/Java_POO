ea71c31ce07963d51f3aeabc7f4675fffa7044b3
Rafael Varago <87142022+rafael-varago@users.noreply.github.com>
2023-01-26 20:11:50 -0300

    Create ex2_fixacao.java

---
+++ introduçãoAPOO/exs_fixacao_sec8_69/ex2/ex2_fixacao.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class ex2_fixacao {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee employee = new Employee();
+		
+		System.out.print("Name: ");
+		employee.name = sc.nextLine();
+		System.out.print("Gross Salary: ");
+		employee.grossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		employee.tax = sc.nextDouble();
+		
+		System.out.println("\nEmployee: " + employee);
+		
+		System.out.print("\nWhich percentage to increase salary? ");
+		double percentage = sc.nextDouble();
+		employee.increaseSalary(percentage);
+		System.out.println("\nUpdated data: " + employee);
+		
+		sc.close();
+
+	}
+
+}
